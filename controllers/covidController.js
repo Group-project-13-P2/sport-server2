b936ba935ff0c22ebd997d1369b30cbdd79848d7
normndakbr <normnd.akbr2@gmail.com>
2020-11-27 02:59:54 +0700

    added endpoints for Covid Data Reports

---
+++ controllers/covidController.js
@@ -0,0 +1,38 @@
+const axios = require('axios')
+
+class covidController {
+    static async getCovidData(request, response, next) {
+        const startDate = request.body.startDate;
+        const today = new Date();
+        let dd = today.getDate();
+        let mm = today.getMonth() + 1;
+        let yyyy = today.getFullYear();
+        if (dd < 10) dd = '0' + dd;
+        if (mm < 10) mm = '0' + mm;
+        const endDate = yyyy + '-' + mm + '-' + dd;
+        try {
+            console.log('Hello from covid controller');
+            console.log(endDate);
+            const { data } = await axios.get(`https://covidtrackerapi.bsg.ox.ac.uk/api/v2/stringency/date-range/${startDate}/${endDate}`);
+            response.status(200).json(data)
+        } catch (error) {
+            console.log(error, 'test');
+            next(error)
+        }
+    }
+
+    static async getCovidDataByDay(request, response, next) {
+        const currentDay = request.body.currentDay;
+        const country = request.body.country;
+        try {
+            console.log('Hello from covid controller');
+            const { data } = await axios.get(`https://covidtrackerapi.bsg.ox.ac.uk/api/v2/stringency/actions/${country}/${currentDay}`);
+            response.status(200).json(data)
+        } catch (error) {
+            console.log(error, 'test');
+            next(error)
+        }
+    }
+}
+
+module.exports = covidController;
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 const route = require("express").Router();
 const userController = require("../controllers/userController");
 const klasemenController = require("../controllers/klasemenController");
+const covidController = require("../controllers/covidController");
 const authentication = require("../middlewares/authentication");
 
 route.post("/register", userController.register);
 route.post("/login", userController.login);
 route.get("/klasemen", klasemenController.getKlasemen);
+route.get("/covidreport", covidController.getCovidData);
+route.get("/covidreportbyday", covidController.getCovidDataByDay);
 route.use(authentication);
 
 module.exports = route;
