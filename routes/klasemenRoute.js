6bdf66e5e09b277bdb3ac1280a0eecb4e7e1cfb4
nafisalfiani <nafisa.alfiani.ica@gmail.com>
2020-11-26 23:58:09 +0700

    separate- klasemenRoute.js

--- routes/klasemenRoute.js
+++ routes/klasemenRoute.js
@@ -1,6 +1,5 @@
 const route = require("express").Router();
 const klasemenController = require("../controllers/klasemenController");
-const authentication = require("../middlewares/authentication");
 
 route.get("/klasemen", klasemenController.getKlasemen);
 route.posts("/klasemen", klasemenController.getKlasemen);
