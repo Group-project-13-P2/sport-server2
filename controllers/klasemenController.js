37ee8e2b6b8055ae031019ea65fabd03612294f0
nafisalfiani <nafisa.alfiani.ica@gmail.com>
2020-11-27 10:21:05 +0700

    filter by Premier League-England

--- controllers/klasemenController.js
+++ controllers/klasemenController.js
@@ -3,7 +3,13 @@ class Klasemen {
     static async getKlasemen(req, res, next) {
         try {
             console.log('object');
-            const { data } = await axios.get("http://api.football-data.org/v2/competitions",
+            const { data } = await axios.get("http://api.football-data.org/v2/competitions/2021/standings",
+            {
+                "headers" : {
+                    "X-Auth-Token" : "efcc91eb520d4b0eb0f58e667e2e93d2",
+                    "area" : "England"
+                }
+            }
             )
             res.status(200).json(data)
         } catch (error) {
